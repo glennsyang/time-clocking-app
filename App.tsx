0b022fee22aeaca65e5adb44aa33b80a120a2d68
Glenn Sheppard <gsheppard.yang@gmail.com>
2022-04-18 15:47:59 -0700

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
+// import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
